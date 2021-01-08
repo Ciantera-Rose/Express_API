0e61c8723ec03b6a17cf53ecc327a050b1480983
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-08 13:49:45 -0500

    virtualizes id instead of _id

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,28 +1,24 @@
 const express = require("express");
 const router = express.Router();
-
 const Todos = require("../models/todoModel");
-
 // GET
 router.get("/todos", (req, res) => {
   Todos.find((err, todos) => {
     if (err) {
-      res
-        .status(404)
-        .json({ message: "Todo not found error", error: `${err}` });
+      res.status(404).json({ message: "Get todos err", error: `${err}` });
     } else {
-      res.status(200).json(todos);
+      res.status(200).json([
+        ...todos.map((todo) => {
+          const { _id, title, done } = todo;
+          return { id: _id, title, done };
+        }),
+      ]);
     }
   });
-  //console.log(req);
-  //res.end();
 });
-
 // POST
-
 router.post("/todo", (req, res) => {
   const todo = new Todos(req.body);
-
   todo
     .save()
     .then((todo) => {
@@ -33,5 +29,4 @@ router.post("/todo", (req, res) => {
       res.status(400).json({ message: "unable to post", errors: `${err}` });
     });
 });
-
 module.exports = router;
