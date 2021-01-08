f55290174f6aaa59f0733b112c8ccb6ffd0d7b5b
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-08 13:38:04 -0500

    add POST and GET Routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -5,8 +5,33 @@ const Todos = require("../models/todoModel");
 
 // GET
 router.get("/todos", (req, res) => {
-  console.log("we out here!");
-  res.end();
+  Todos.find((err, todos) => {
+    if (err) {
+      res
+        .status(404)
+        .json({ message: "Todo not found error", error: `${err}` });
+    } else {
+      res.status(200).json(todos);
+    }
+  });
+  //console.log(req);
+  //res.end();
+});
+
+// POST
+
+router.post("/todo", (req, res) => {
+  const todo = new Todos(req.body);
+
+  todo
+    .save()
+    .then((todo) => {
+      const { _id, title, done } = todo;
+      res.status(200).json({ id: _id, title, done });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "unable to post", errors: `${err}` });
+    });
 });
 
 module.exports = router;
