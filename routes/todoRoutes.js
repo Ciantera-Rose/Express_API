80ccd1bb13b37c9e8de7bbb64653a6f6c972242d
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-08 14:07:08 -0500

    adds DELETE route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -50,4 +50,15 @@ router.patch("/todo/:id", (req, res) => {
     }
   });
 });
+
+// DELETE
+router.delete("/delete/todo/:id", (req, res) => {
+  Todos.findByIdAndRemove(req.params.id, (err, todo) => {
+    if (err) {
+      res.status(404).json({ message: "Could not delete", errors: `${err}` });
+    } else {
+      res.status(200).json({ message: "Deleted todo" });
+    }
+  });
+});
 module.exports = router;
